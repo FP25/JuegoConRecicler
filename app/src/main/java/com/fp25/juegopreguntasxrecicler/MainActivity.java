7d83e80dbf6eb7098de5dc69f0d37a1070780453
Yo_Fp <yo@correo.com>
2026-01-11 23:33:31 +0100

    Guardado D finde

--- app/src/main/java/com/fp25/juegopreguntasxrecicler/MainActivity.java
+++ app/src/main/java/com/fp25/juegopreguntasxrecicler/MainActivity.java
@@ -1,14 +1,30 @@
 package com.fp25.juegopreguntasxrecicler;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<EventModel>eventosHistoricos=new ArrayList<>();
+    String respuesta="";
+    int cont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,45 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        RecyclerView rview=findViewById(R.id.eventsRecicler);
+
+        AdaptadorDEventos adapter=new AdaptadorDEventos(this,eventosHistoricos);
+        rview.setAdapter(adapter);
+        rview.setLayoutManager(new LinearLayoutManager(this));
+
+        juegoPreguntados();
+
+    }
+
+    public void juegoPreguntados() {
+        String[] eventosNombres = getResources().getStringArray(R.array.historic_event_names);
+        String[] eventosFechas = getResources().getStringArray(R.array.historic_event_dates);
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        for (int i = 0; i <eventosNombres.length; i++){
+            cont =i;
+            LayoutInflater inflador = getLayoutInflater();
+            View dialogCustom = inflador.inflate(R.layout.dialog_custom, null);
+
+            alert.setTitle(eventosNombres[0]);
+            alert.setMessage("En que año surgio? ");
+            alert.setCancelable(false);
+            TextInputLayout respuestaLY = dialogCustom.findViewById(R.id.dialog_Txt);
+            alert.setPositiveButton(
+                    "Verificar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            respuesta = respuestaLY.getEditText().getText().toString();
+                            if (respuesta.equals(eventosFechas[cont])){
+                                Log.d("","coorrectogggggggg");
+                                cont=eventosNombres.length-1;
+                            }
+                        }
+                    });
+            alert.setView(dialogCustom);
+            alert.show();
+
+        }
     }
 }
